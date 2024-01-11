49a3b8d664d2fa9c32f757739d5857bd24c82062
3FANG <d.hasimikov@mail.ru>
2024-01-11 20:13:40 +0300

    Change setup.cfg and remove logic from tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,1 @@
-from .test_validate import(
-    test_required,
-    test_instantiation
-)
-from .test_string import (
-    test_contains,
-    test_priorities
-)
-from .test_numbers import (
-    test_positive,
-    test_range
-)
-from .test_list import test_sizeof_list
-from .test_dict import test_shape
+"""Module with tests Validator functional."""
