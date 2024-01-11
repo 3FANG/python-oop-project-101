91174aeb54919be44ca7bf06bbe0139db45d21ce
3FANG <d.hasimikov@mail.ru>
2024-01-11 12:12:03 +0300

    Add custom validation functional and change the class structure

--- hexlet_code/datatypes.py
+++ hexlet_code/datatypes.py
@@ -1,56 +1,100 @@
+from typing import NamedTuple, Optional
+from collections.abc import Callable
+
+
+class CheckFunction(NamedTuple):
+    """Сlass representing a single condition"""
+    check_function: Callable[[int], bool] | Callable[[str], bool]
+    args: list[Optional[int | str]]
+
+
 class DataType:
-    def __init__(self):
-        self.conditions = {}
+    """Base class for data type"""
+    def __init__(self, validator):
+        self.validator = validator
+        self.conditions: dict[str, CheckFunction] = {}
+        self.name = None
 
     '''
     Check the specified string for compliance with each condition
-    by substituting a string as an argument into an anonymous function
+    by substituting a value as an argument into a check function
     '''
-    def is_valid(self, string):
-        return all(map(lambda check_function: check_function(string), self.conditions.values()))
+    def is_valid(self, value):
+        for check_function, args in self.conditions.values():
+            if not check_function(value, *args):
+                return False
+        return True
+
+    '''
+    Takes a custom validation function from a Validator object
+    by specified name and adds it to the conditions dictionary
+    '''
+    def test(self, name, value):
+        condition: CheckFunction = self.validator.custom_validators[self.name][name]
+        condition.args.append(value)
+        self.conditions[name] = condition
+        return self
 
 
 class String(DataType):
+    def __init__(self, validator):
+        super().__init__(validator)
+        self.name = 'string'
+
     def required(self):
-        self.conditions['required'] = lambda string: string not in (None, '')
+        self.conditions['required'] = CheckFunction(check_function=lambda string: string not in (None, ''), args=[])
         return self
 
     def contains(self, template):
-        self.conditions['contains'] = lambda string: template in string
+        self.conditions['contains'] = CheckFunction(check_function=lambda string: template in string, args=[])
         return self
 
     def min_len(self, length):
-        self.conditions['min_len'] = lambda string: len(string) >= length
+        self.conditions['min_len'] = CheckFunction(check_function=lambda string: len(string) >= length, args=[])
         return self
 
 
 class Number(DataType):
+    def __init__(self, validator):
+        super().__init__(validator)
+        self.name = 'number'
+
     def required(self):
-        self.conditions['required'] = lambda number: isinstance(number, int)
+        self.conditions['required'] = CheckFunction(check_function=lambda number: isinstance(number, int), args=[])
         return self
 
     def positive(self):
-        self.conditions['positive'] = lambda number: True if number is None else number > 0
+        self.conditions['positive'] = CheckFunction(check_function=lambda number: True if number is None else number > 0, args=[])
         return self
 
     def range(self, start, stop):
-        self.conditions['range'] = lambda number: start <= number <= stop
+        self.conditions['range'] = CheckFunction(check_function=lambda number: start <= number <= stop, args=[])
         return self
 
 
 class ListData(DataType):
+    def __init__(self, validator):
+        super().__init__(validator)
+        self.name = 'list'
+
     def required(self):
-        self.conditions['required'] = lambda list_of_data: type(list_of_data) == list
+        self.conditions['required'] = CheckFunction(check_function=lambda list_of_data: type(list_of_data) == list, args=[])
         return self
 
     def sizeof(self, size):
-        self.conditions['sizeof'] = lambda list_of_data: len(list_of_data) == size
+        self.conditions['sizeof'] = CheckFunction(check_function=lambda list_of_data: len(list_of_data) == size, args=[])
         return self
 
 
 class DictData(DataType):
-    def shape(self, scheme: dict):
-        self.conditions.update(scheme)
+    def __init__(self, validator):
+        super().__init__(validator)
+        self.conditions: dict[str, DataType] = {}
+        self.name = 'dict'
+
+    def shape(self, schema: dict):
+        self.conditions.update(schema)
+        print(self.conditions)
         return self
 
     def is_valid(self, map_of_data: dict):
--- hexlet_code/validate.py
+++ hexlet_code/validate.py
@@ -1,18 +1,27 @@
-from hexlet_code.datatypes import String, Number, ListData, DictData
+from hexlet_code.datatypes import String, Number, ListData, DictData, CheckFunction
 
 
 class Validator:
     def __init__(self):
-        self.conditions = {}
+        self.datatypes = {
+            'string': String,
+            'number': Number,
+            'list': ListData,
+            'dict': DictData
+        }
+        self.custom_validators = {datatype: {} for datatype in self.datatypes.keys()}
 
     def string(self):
-        return String()
+        return self.datatypes['string'](self)
 
     def number(self):
-        return Number()
+        return self.datatypes['number'](self)
 
     def list(self):
-        return ListData()
+        return self.datatypes['list'](self)
 
     def dict(self):
-        return DictData()
+        return self.datatypes['dict'](self)
+
+    def add_validator(self, type, name, fn):
+        self.custom_validators[type][name] = CheckFunction(check_function=fn, args=[])
